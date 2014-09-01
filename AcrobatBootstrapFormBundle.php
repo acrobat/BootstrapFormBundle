2954f147483ade0afa3768547a1313c5b3374210
Jeroen Thora <jeroen.thora@cognosis.be>
2014-09-01 19:58:07 +0200

    basic bundle files

---
+++ AcrobatBootstrapFormBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Acrobat\Bundle\BootstrapFormBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * AcrobatBootstrapFormBundle
+ *
+ * @author Jeroen Thora <jeroenthora@gmail.com>
+ */
+class AcrobatBootstrapFormBundle extends Bundle
+{
+
+}
